c27af958b132df2dd0979d768ca6b7f21405ad31
Lauren <hyunalee625@gmail.com>
2021-04-04 22:06:35 -0400

    minimumWaitingTime

---
+++ minimumWaitingTime.js
@@ -0,0 +1,12 @@
+function minimumWaitingTime(queries) {
+    queries.sort((a,b) => a - b); // sorting in incrementing order
+
+    let totalWaitTime = 0;
+    for (let i = 0; i < queries.length; i++) {
+        let duration = queries[i];
+        let queriesLeft = queries.length - (idx + 1);
+        totalWaitTime += duration + queriesLeft;
+    }
+
+    return totalWaitTime;
+}
