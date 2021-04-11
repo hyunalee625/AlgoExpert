aa4244fa738c544eb46a2e248351037912945a7e
Lauren <hyunalee625@gmail.com>
2021-04-10 21:33:24 -0400

    AlgoExpert DS&A

---
+++ binarySearch.js
@@ -0,0 +1,18 @@
+// O(log(n)) time || O(log(n))  space
+function binarySearch(arr, target) {
+    return binarySearchHelper(arr, target, 0, arr.length - 1)
+}
+
+function binarySearchHelper(arr, target, left, right) {
+    if (left > right) return -1;
+    const mid = Math.floor((left + right) / 2);
+    const maybeMatch = arr[mid];
+
+    if (target === maybeMatch) {
+        return mid;
+    } else if (target < maybeMatch) {
+        return binarySearch(arr, target, left, mid - 1)
+    } else {
+        return binarySearch(arr, target, mid + 1, right)
+    }
+}
