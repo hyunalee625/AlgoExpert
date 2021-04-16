08021c2076ff108be53cfb99c9a5e0a5e9ab4451
Lauren <hyunalee625@gmail.com>
2021-04-15 20:54:36 -0400

    DS&A questions

---
+++ runLengthEncoding.js
@@ -0,0 +1,22 @@
+function runLengthEncoding(str) {
+    const encodedStrChar = [];
+    let currentLength = 1;
+
+    for (let i = 1; i< str.length; i++) {
+        const currentChar = str[i];
+        const prevChar = str[i - 1];
+
+        if (currentChar !== prevChar || currentLength === 9) {
+            encodedStrChar.push(currentLength.toString());
+            encodedStrChar.push(prevChar);
+            currentLength = 0;
+        }
+
+        currentLength++;
+    }
+
+    encodedStrChar.push(currentLength.toString());
+    encodedStrChar.push(str[str.length - 1]);
+
+    return encodedStrChar.join('')
+}
